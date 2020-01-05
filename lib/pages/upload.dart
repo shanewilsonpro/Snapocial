a5cb52dc1639939aefd246db6f8e7d6d222d82ea
Shane <shanewilsonpro@gmail.com>
2020-01-04 20:45:13 -0800

    add image picker function

--- lib/pages/upload.dart
+++ lib/pages/upload.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Upload extends StatefulWidget {
   @override
@@ -7,7 +10,50 @@ class Upload extends StatefulWidget {
 }
 
 class _UploadState extends State<Upload> {
-  selectImage(context) {}
+  File file;
+
+  handleTakePhoto() async {
+    Navigator.pop(context);
+    File file = await ImagePicker.pickImage(source: ImageSource.camera, 
+    maxHeight: 675, 
+    maxWidth: 960,);
+    setState(() {
+      this.file = file;
+    });
+  }
+
+  handleChooseFromGallery() async {
+    Navigator.pop(context);
+    File file = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      this.file = file;
+    });
+  }
+
+  selectImage(parentContext) {
+    return showDialog(
+      context: parentContext,
+      builder: (context) {
+        return SimpleDialog(
+          title: Text("Create Post"),
+          children: <Widget>[
+            SimpleDialogOption(
+              child: Text("Photo with Camera"),
+              onPressed: handleTakePhoto
+            ),
+            SimpleDialogOption(
+              child: Text("Image from Gallery"),
+              onPressed: handleChooseFromGallery
+            ),
+            SimpleDialogOption(
+              child: Text("Cancel"),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ],
+        );
+      }
+    );
+  }
 
   Container buildSplashScreen() {
     return Container(
@@ -30,7 +76,7 @@ class _UploadState extends State<Upload> {
                 ),
               ),
               color: Colors.deepOrange,
-              onPressed: () => selectImage(context),
+              onPressed: () => selectImage(context)
             ),
           ),
         ],
@@ -38,8 +84,12 @@ class _UploadState extends State<Upload> {
     );
   }
 
+  buildUploadForm() {
+    return Text("File loaded");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return buildSplashScreen();
+    return file == null ? buildSplashScreen() : buildUploadForm();
   }
 }
