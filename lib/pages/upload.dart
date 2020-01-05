8c967f07d7b84f08c7e24440d0c18c13e5c7bc3e
Shane <shanewilsonpro@gmail.com>
2020-01-04 19:48:38 -0800

    upload screen on upload page

--- lib/pages/upload.dart
+++ lib/pages/upload.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Upload extends StatefulWidget {
   @override
@@ -6,8 +7,39 @@ class Upload extends StatefulWidget {
 }
 
 class _UploadState extends State<Upload> {
+  selectImage(context) {}
+
+  Container buildSplashScreen() {
+    return Container(
+      color: Theme.of(context).accentColor.withOpacity(0.6),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SvgPicture.asset('assets/images/upload.svg', height: 260.0),
+          Padding(
+            padding: EdgeInsets.only(top: 20.0),
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Text(
+                "Upload Image",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
+                ),
+              ),
+              color: Colors.deepOrange,
+              onPressed: () => selectImage(context),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Upload");
+    return buildSplashScreen();
   }
 }
