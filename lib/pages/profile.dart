a0515bce6ab4323685f4b7d27ec2e7152f84634a
Shane <shanewilsonpro@gmail.com>
2020-01-09 13:09:25 -0800

    adding edit profile button

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -15,6 +15,8 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  final String currentUserId = currentUser?.id;
+
   Column buildCountColumn(String label, int count) {
     return Column(
       mainAxisSize: MainAxisSize.min,
@@ -38,8 +40,48 @@ class _ProfileState extends State<Profile> {
       ],
     );
   }
+
+  editProfile() {
+
+  }
+
+  Container buildButton({ String text, Function function }) {
+    return Container(
+      padding: EdgeInsets.only(top: 2.0),
+      child: FlatButton(
+        onPressed: function,
+        child: Container(
+          width: 250.0,
+          height: 26.0,
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            border: Border.all(
+              color: Colors.blue,
+            ),
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+        ),
+      ),
+    );
+  }
+
   buildProfileButton() {
-    return Text("profile button");
+    //viewing your own profile - should show edit profile button
+    bool isProfileOwner = currentUserId == widget.profileId;
+    if (isProfileOwner) {
+      return buildButton(
+        text: "Edit Profile",
+        function: editProfile
+      );
+    }
   }
 
   buildProfileHeader() {
