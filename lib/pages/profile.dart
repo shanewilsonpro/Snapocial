f0d1a5bea0b7e5f27fd2aacc0c7f5287aeb8cdf6
Shane <shanewilsonpro@gmail.com>
2020-01-09 22:25:19 -0800

    displaying post and post count

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,9 +1,11 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:snapocial/models/user.dart';
 import 'package:snapocial/pages/edit_profile.dart';
 import 'package:snapocial/pages/home.dart';
 import 'package:snapocial/widgets/header.dart';
+import 'package:snapocial/widgets/post.dart';
 import 'package:snapocial/widgets/progress.dart';
 
 class Profile extends StatefulWidget {
@@ -17,6 +19,33 @@ class Profile extends StatefulWidget {
 
 class _ProfileState extends State<Profile> {
   final String currentUserId = currentUser?.id;
+  bool isLoading = false;
+  int postCount = 0;
+  List<Post> posts = [];
+
+  @override
+  void initState() { 
+    super.initState();
+    getProfilePosts();
+  }
+
+  getProfilePosts() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    QuerySnapshot snapshot = await postsRef
+      .document(widget.profileId)
+      .collection('userPosts')
+      .orderBy('timestamp', descending: true)
+      .getDocuments();
+    setState(() {
+      isLoading = false;
+      postCount = snapshot.documents.length;
+      posts = snapshot.documents.map((doc) => Post.fromDocument(doc))
+      .toList();
+    });
+  }
 
   Column buildCountColumn(String label, int count) {
     return Column(
@@ -113,7 +142,7 @@ class _ProfileState extends State<Profile> {
                           mainAxisSize: MainAxisSize.max,
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
-                            buildCountColumn("posts", 0),
+                            buildCountColumn("posts", postCount),
                             buildCountColumn("followers", 0),
                             buildCountColumn("following", 0),
                           ],
@@ -164,12 +193,27 @@ class _ProfileState extends State<Profile> {
     );
   }
 
+  buildProfilePosts() {
+    if (isLoading) {
+      return circularProgress();
+    }
+    return Column(
+      children: posts,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: header(context, titleText: "Profile"),
       body: ListView(
-        children: <Widget>[buildProfileHeader()],
+        children: <Widget>[
+          buildProfileHeader(),
+          Divider(
+            height: 0.0,
+          ),
+          buildProfilePosts(),
+          ],
       ),
     );
   }
