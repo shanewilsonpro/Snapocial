c15053b209da2829c590c0dd09cc949b88d78807
Shane <shanewilsonpro@gmail.com>
2020-02-03 20:24:36 -0800

    creating onCreateFollower firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +8,36 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+exports.onCreateFollower = functions.firestore
+    .document("/followers/{userId}/userFollowers/{followerId}")
+    .onCreate(async (snapshot, context) => {
+        console.log("Follower Created", snapshot.id);
+        const userId = context.params.userId;
+        const followerId = context.params.followerId;
+
+        //get followed users posts
+        const followedUserPostsRef = admin
+            .firestore()
+            .collection('posts')
+            .doc(userId)
+            .collection('userPosts')
+
+        //get following user's timeline
+        const timelinePostsRef = admin 
+            .firestore()
+            .collection('timeline')
+            .doc(followerId)
+            .collection('timelinePosts');
+        
+        //get followed users posts
+        const querySnapshot = await followedUserPostsRef.get();
+
+        //add each user post to following user's timeline
+        querySnapshot.forEach(doc => {
+            if (doc.exists) {
+                const postId = doc.id;
+                const postData = doc.data();
+                timelinePostsRef.doc(postId).set(postData);
+            }
+        })
+    });
